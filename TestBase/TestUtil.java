1a4e6372cc39fb7627c34d78ffdbd88f6e743ada
Gunsanjam Kaur <97036375+GunsanjamInfostride@users.noreply.github.com>
2022-02-14 12:55:42 +0530

    Add files via upload

---
+++ TestBase/TestUtil.java
@@ -0,0 +1,9 @@
+package TestBase;
+
+public class TestUtil 
+{
+	public static long PAGE_LOAD_TIMEOUT = 10000;
+	public static long IMPLICITY_WAIT = 10000;
+	public static long IMPLI_WAIT=3000;
+
+}
